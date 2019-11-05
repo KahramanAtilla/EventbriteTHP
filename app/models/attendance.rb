70efc7b341fee299482210e01870851e2d14bb49
Atilla <atillafacebook@hotmail.fr>
2019-11-05 13:01:21 +0100

    navbar missing, bootsrap missing

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,12 @@
 class Attendance < ApplicationRecord
+  #after_create :welcome_sendo
+
   belongs_to :user
   belongs_to :event
   validates :stripe_customer_id, uniqueness: true
+
+
+  #def welcome_sendo
+  #  UserMailer.welcome_email(self).deliver_now
+  #end
 end
--- db/migrate/20191105094158_create_users.rb
+++ db/migrate/20191105094158_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :first_name
       t.string :last_name
       t.text :description
+
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
