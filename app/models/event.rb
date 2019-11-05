e4b1842484ddaec3f13c825a314b51f284c93e87
Atilla <atillafacebook@hotmail.fr>
2019-11-05 10:47:25 +0100

    migrations done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,12 @@
 class Event < ApplicationRecord
+  has_many: attendances
+  has_many: users, through: :attendances
+ 
   validates :start_date, presence: true
   validates :duration, presence: true
   validates :title, presence: :true, length: { in: 5..140 }
   validates :description, presence: true, length: { in: 20..1000 }
-  validates :price, presence: true,
+  validates :price, presence: true, less_than_or_equal_to: 1000, greater_than: 1
   validates :location, presence: true
 
 
