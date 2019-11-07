c66d1ac60ccbb3c275a34486cb3e8e6dd7e8e509
Atilla <atillafacebook@hotmail.fr>
2019-11-07 10:03:02 +0100

    stripemothafucka

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.1'
 gem 'dotenv-rails'
 gem 'table_print'
 gem 'devise'
+gem 'stripe'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'events_controller#index#index'
+  resources :charges
   get '/events_controller/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
