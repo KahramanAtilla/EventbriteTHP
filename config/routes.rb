abe594827114ec24ffbf7f51d88ab804379e3f4d
Atilla <atillafacebook@hotmail.fr>
2019-11-07 12:26:46 +0100

    pushing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :charges
   get '/events_controller/index'
   devise_for :users
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
